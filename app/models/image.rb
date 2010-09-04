ad6820fcdd9bfba242b0ac87e0f066257b51ce82
Anna <tanna22@gmail.com>
2010-09-04 01:13:39 -0400

    added file upload

---
+++ app/models/image.rb
@@ -0,0 +1,17 @@
+class Image < ActiveRecord::Base
+	def self.save(upload)
+		name = upload['image'].original_filename
+		directory = "public/data"
+		# create the file path
+		path = File.join(directory, name)
+		# write the file
+		File.open(path, "wb") { |f| f.write(upload['image'].read) }
+	end
+	def sanitize_filename(file_name)
+		# get only the filename, not the whole path (from IE)
+		just_filename = File.basename(file_name) 
+		# replace all none alphanumeric, underscore or perioids
+		# with underscore
+		just_filename.sub(/[^\w\.\-]/,'_') 
+	end
+end
